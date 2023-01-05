d4543343323df790a3ad9135c20f0fddc3c5d236
Deor0 <117356415+Deor0@users.noreply.github.com>
2023-01-05 14:55:52 +0545

    Password-generator

---
+++ password-generator/script.js
@@ -0,0 +1,79 @@
+const pwEl = document.getElementById('pw')
+const copyEl = document.getElementById('copy')
+const lengthEl = document.getElementById('length')
+const upperEl = document.getElementById('upper')
+const lowerEl = document.getElementById('lower')
+const numberEl = document.getElementById('number')
+const symbolEl = document.getElementById('symbol')
+const generateEl = document.getElementById('generate')
+
+
+
+const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerLetters = 'abcdefghijklmnopqrstuvwxyz'
+const numbers = '0123456789'
+const symbols = '!@#$%^&*()_+=`~;:'
+
+// Generate random elements
+
+function getLowerCase() {
+    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)]
+}
+
+function getUpperCase() {
+    return upperLetters[Math.floor(Math.random() * upperLetters.length)]
+}
+
+function getNumber() {
+    return numbers[Math.floor(Math.random() * numbers.length)]
+}
+
+function symbol() {
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+
+function generatePassword() {
+    const length = lengthEl.value;
+    let password = '';
+    if (!length) {
+        alert('Please provide a length')
+    } else {
+        for (let i = 0; i < length; i++) {
+            const x = generateX();
+
+            password += x
+            console.log(x)
+        }
+        pwEl.innerText = password
+    }
+
+}
+
+generateEl.addEventListener('click', generatePassword)
+
+
+function generateX() {
+    const xs = [];
+
+    if (upperEl.checked) {
+        xs.push(getUpperCase())
+    }
+    if (lowerEl.checked) {
+        xs.push(getLowerCase())
+    }
+    if (numberEl.checked) {
+        xs.push(getNumber())
+    }
+    if (symbolEl.checked) {
+        xs.push(symbol())
+    }
+    console.log(xs)
+    return xs[Math.floor(Math.random() * xs.length)];
+}
+
+// Clip board
+
+copyEl.addEventListener('click', () => {
+    navigator.clipboard.writeText(pwEl.innerText);
+})
